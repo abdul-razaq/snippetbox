0949c08989c9da41c84c0e8358243b4c8863c097
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2023-10-12 10:06:37 +0100

    created error helper methods and refactored handlers to use the methods

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "The HTTP network address the server should listen on")
 
@@ -15,20 +20,25 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	
+
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", viewSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.viewSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	server := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler:  mux,
 	}
 
 	infoLog.Printf("Server listening on %s", *addr)
